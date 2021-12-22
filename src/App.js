a12c3803f150e555b0d8a1bd4c8c112609889244
Yassin Maher <professor.yassin@yahoo.com>
2021-12-22 15:35:25 +0200

    Adding Editor Config

--- src/App.js
+++ src/App.js
@@ -1,57 +1,59 @@
-import GlobalStyles from './components/GlobalStyles'
-import styled, { ThemeProvider } from 'styled-components'
-import logo from './images/logo.svg'
-import iconPerson from './images/icon-person.svg'
-import iconDollar from './images/icon-dollar.svg'
-import InputField from './components/InputField'
-import Tip from './components/Tip'
-import theme from './config/theme'
+import GlobalStyles from "./components/GlobalStyles";
+import styled, { ThemeProvider } from "styled-components";
+import logo from "./images/logo.svg";
+import iconPerson from "./images/icon-person.svg";
+import iconDollar from "./images/icon-dollar.svg";
+import InputField from "./components/InputField";
+import Tip from "./components/Tip";
+import theme from "./config/theme";
 
 const App = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <img src={logo} alt='logo' />
-      <StyledContainer>
-        <form>
-          <label>Bill</label>
-          <InputField icon={iconDollar} />
-        </form>
-        <form>
-          <label>Select Tip %</label>
-          <div className='tipContainer'>
-            <Tip value='5' select={true} />
-            <Tip value='10' />
-            <Tip value='15' />
-            <Tip value='25' />
-            <Tip value='35' />
-            <Tip value='custom' />
-          </div>
-        </form>
-        <form>
-          <label>Number of Persons</label>
-          <InputField icon={iconPerson} />
-        </form>
-      </StyledContainer>
-    </ThemeProvider>
-  )
-}
+    return (
+        <ThemeProvider theme={theme}>
+            <GlobalStyles />
+            <img src={logo} alt="logo" />
+            <StyledContainer>
+                <form>
+                    <div className="form-group">
+                        <label>Bill</label>
+                        <InputField icon={iconDollar} />
+                    </div>
+                    <div className="form-group">
+                        <label>Select Tip %</label>
+                        <div className="tipContainer">
+                            <Tip value="5" select={true} />
+                            <Tip value="10" />
+                            <Tip value="15" />
+                            <Tip value="25" />
+                            <Tip value="35" />
+                            <Tip value="custom" />
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <label>Number of Persons</label>
+                        <InputField icon={iconPerson} />
+                    </div>
+                </form>
+            </StyledContainer>
+        </ThemeProvider>
+    );
+};
 
 const StyledContainer = styled.div`
-  background-color: ${({ theme }) => theme.colors.white};
-  margin-top: 30px;
-  padding: 20px;
-  border-radius: 20px;
-  text-align: left;
-  .tipContainer {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-gap: 10px;
-    margin-top: 5px;
-  }
-  form {
-    margin-bottom: 10px;
-  }
-`
+    background-color: ${({ theme }) => theme.colors.white};
+    margin-top: 30px;
+    padding: 20px;
+    border-radius: 20px;
+    text-align: left;
+    .tipContainer {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-gap: 10px;
+        margin-top: 5px;
+    }
+    .form-group {
+        margin-bottom: 10px;
+    }
+`;
 
-export default App
+export default App;
